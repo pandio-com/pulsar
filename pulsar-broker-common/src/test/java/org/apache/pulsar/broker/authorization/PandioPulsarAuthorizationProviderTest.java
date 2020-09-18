cf53bee02fa337c658050108c2d53cd3d2d49416
charvak <charvak@pandio.com>
2020-09-18 20:06:11 +0530

    PND-655 Auth Provider Fix

---
+++ pulsar-broker-common/src/test/java/org/apache/pulsar/broker/authorization/PandioPulsarAuthorizationProviderTest.java
@@ -0,0 +1,47 @@
+package org.apache.pulsar.broker.authorization;
+
+import lombok.val;
+import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.common.naming.TopicName;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.ExecutionException;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class PandioPulsarAuthorizationProviderTest {
+
+    @Test
+    public void testSuperAdmin() throws ExecutionException, InterruptedException {
+        val sut = new PandioPulsarAuthorizationProvider.Permissions(true, Collections.EMPTY_LIST, null, null);
+        assertTrue(sut.checkAdmin("gib").get());
+        assertTrue(sut.checkAdmin(NamespaceName.get("public", "default" )).get());
+        assertTrue(sut.checkAdmin(TopicName.get("persistent", "public", "default", "test")).get());
+    }
+
+    @Test
+    public void testProduceAccess() throws ExecutionException, InterruptedException {
+        val sut = new PandioPulsarAuthorizationProvider.Permissions(false, null, null, Collections.singletonList("blah"));
+        assertFalse(sut.checkAdmin("gib").get());
+        assertFalse(sut.checkAdmin("blah").get());
+        assertFalse(sut.checkConsume("blah").get());
+        assertTrue(sut.checkProduce("blah").get());
+        assertFalse(sut.checkProduce(NamespaceName.get("public", "default" )).get());
+        assertFalse(sut.checkAdmin(TopicName.get("persistent", "public", "default", "test")).get());
+    }
+
+    @Test
+    public void testProduceAccessNegative() throws ExecutionException, InterruptedException {
+        val sut = new PandioPulsarAuthorizationProvider.Permissions(false, null, null, Collections.singletonList("public"));
+        assertFalse(sut.checkAdmin("gib").get());
+        assertFalse(sut.checkAdmin("blah").get());
+        assertFalse(sut.checkConsume("blah").get());
+        assertFalse(sut.checkProduce("blah").get());
+        assertTrue(sut.checkProduce("public").get());
+        assertTrue(sut.checkProduce(NamespaceName.get("public", "default" )).get());
+        assertTrue(sut.checkProduce(TopicName.get("persistent", "public", "default", "test")).get());
+    }
+}
