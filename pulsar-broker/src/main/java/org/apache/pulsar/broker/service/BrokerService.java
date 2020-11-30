933439bff1b4991cd2479b2e773cf71174776b94
Munir Khakhi <munir131@users.noreply.github.com>
2020-11-30 20:08:36 +0530

    Added proxyServiceUrl for replication producer (#21)
    * PND-1067 #comment Added new flag for replication TLS

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/service/BrokerService.java
@@ -122,6 +122,7 @@ import org.apache.pulsar.client.admin.PulsarAdmin;
 import org.apache.pulsar.client.admin.PulsarAdminBuilder;
 
 import org.apache.pulsar.client.api.ClientBuilder;
+import org.apache.pulsar.client.api.ProxyProtocol;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.PulsarClientException;
 import org.apache.pulsar.client.impl.ClientBuilderImpl;
@@ -893,10 +894,12 @@ public class BrokerService implements Closeable, ZooKeeperCacheListener<Policies
                     clientBuilder.authentication(pulsar.getConfiguration().getBrokerClientAuthenticationPlugin(),
                             pulsar.getConfiguration().getBrokerClientAuthenticationParameters());
                 }
-                if (pulsar.getConfiguration().isBrokerClientTlsEnabled()) {
+                if (pulsar.getConfiguration().isBrokerClientTlsEnabled() || pulsar.getConfiguration().isReplicationTlsEnabled()) {
+                    String url = isNotBlank(data.getBrokerServiceUrlTls()) ? data.getBrokerServiceUrlTls()
+                                    : data.getServiceUrlTls();
                     clientBuilder
-                            .serviceUrl(isNotBlank(data.getBrokerServiceUrlTls()) ? data.getBrokerServiceUrlTls()
-                                    : data.getServiceUrlTls())
+                            .serviceUrl(url)
+                            .proxyServiceUrl(url, ProxyProtocol.SNI)
                             .enableTls(true)
                             .allowTlsInsecureConnection(pulsar.getConfiguration().isTlsAllowInsecureConnection());
                     if (pulsar.getConfiguration().isBrokerClientTlsEnabledWithKeyStore()) {
