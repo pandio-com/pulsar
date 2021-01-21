5bc0f9f8c9386761a8edbf1acfa63af75e54d962
Victor Oginskis <victor@pandio.com>
2021-01-21 17:16:18 +0200

    experimental: k8s namespace per tenant
    * Changed format
    * fixing: https://stackoverflow.com/questions/65896334/python-pip-broken-wiith-sys-stderr-writeferror-exc

---
+++ pulsar-functions/runtime/src/main/java/org/apache/pulsar/functions/runtime/kubernetes/PandioKubernetesManifestCustomizer.java
@@ -0,0 +1,38 @@
+package org.apache.pulsar.functions.runtime.kubernetes;
+
+import io.kubernetes.client.models.V1Service;
+import io.kubernetes.client.models.V1StatefulSet;
+import org.apache.pulsar.functions.proto.Function;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PandioKubernetesManifestCustomizer implements KubernetesManifestCustomizer {
+
+    public PandioKubernetesManifestCustomizer() {
+
+    }
+
+    @Override
+    public void initialize(Map<String, Object> runtimeCustomizerConfig) {
+
+    }
+
+    @Override
+    public V1StatefulSet customizeStatefulSet(Function.FunctionDetails funcDetails, V1StatefulSet statefulSet) {
+        return statefulSet;
+    }
+
+    @Override
+    public V1Service customizeService(Function.FunctionDetails funcDetails, V1Service service) {
+        return service;
+    }
+
+    @Override
+    public String customizeNamespace(Function.FunctionDetails funcDetails, String currentNamespace) {
+        Pattern nsPattern = Pattern.compile("(.+)--(.+)-fn", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = nsPattern.matcher(currentNamespace);
+        return String.format("%s--%s--%s-fn", matcher.group(1), matcher.group(2), funcDetails.getTenant());
+    }
+}
